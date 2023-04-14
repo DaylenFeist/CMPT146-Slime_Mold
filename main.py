2d1b5b2eac20c18176a4c7541a1093a5d551aa74
DaylenFeist <daylen.feist1@gmail.com>
2023-04-14 14:05:55 -0600

    Added framework for Agents: -Created file for class -Created Class -Created movement, which moves in the agent's direction, if it is outside the bounding box, the direction will be randomly changed, and agent will be placed at the nearest edge.

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ Description:
         If I decide to change these rules, I will leave the original rule in, and make
             an amendment
         The rules that I created were heavily based on Sage Jenson's slime mold simulation
-            which is the top sources. Thus, it is not exactly the perfect analog to the
+            which is the top source. Thus, it is not exactly the perfect analog to the
             real life slime mold, which is often seen branching out. However, it does
             a beautiful spectacle to behold!
 
@@ -33,34 +33,51 @@ Description:
 
 from PIL import Image
 import numpy as np
-import random
+import Agent as ag
 
 WIDTH, HEIGHT = (50, 50)
 TOTAL_FRAMES = 100
+MOVE_SPEED = 1
+NUM_AGENTS = 10
 #MAIN LOOP
 def main():
+    """Added
+    framework
+    for Agents:
+        -Created
+        Class
+    -Created
+    movement, which
+    moves in the
+    agent
+    's direction, if it is outside the bounding box, the direction will be created"""
     """
     Purpose: This function's primary objective is to house all of the primary functions
     of the project.
         This includes creating the background, all the agents, creating each frame, and
         creating the gif
     Pre-Conditions: none
-    Post-Contitions: creates gif in same directiory
+    Post-Conditions: creates gif in same directory
     Return: Returns nothing
     """
     #create trail_map
     trail_map =np.zeros((HEIGHT,WIDTH), dtype=np.uint8)
 
     #create all agents
-        #add code here
+    agents = []
+    for x in range(NUM_AGENTS):
+        agents.append(ag.Agent(WIDTH,HEIGHT))
 
     #simulate frames,
     frames = []
     for i in range(TOTAL_FRAMES):
-
-
+        for single_agent in agents:
+            single_agent.move_forward(WIDTH,HEIGHT,MOVE_SPEED)
+        
         #convert numpy array to pillow Image
         frames.append(Image.fromarray(trail_map))
 
     #convert all frames to a gif
     frames[0].save('trail_map.gif', format="GIF", append_images=frames[1:], save_all=True, loop=0)
+
+main()
